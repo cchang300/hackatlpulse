8272c4bee50a3118bb0b3a4b4476bb6d54536a86
Daphne Chen <daphnechen@users.noreply.github.com>
2015-10-16 21:40:57 -0400

    pulse.py
    initial python file

---
+++ pulse.py
@@ -0,0 +1,36 @@
+# Pulse 
+# open source imported from https://people.csail.mit.edu/hubert/pyaudio/#examples
+
+import pyaudio
+import wave
+import sys
+
+#pyaudio.PyAudio()
+#__init__()
+#pyaudio.PyAudio.open()
+
+CHUNK = 1024
+
+if len(sys.argv) < 2:
+    print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
+    sys.exit(-1)
+
+wf = wave.open(sys.argv[1], 'rb')
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+
+data = wf.readframes(CHUNK)
+
+while data != '':
+    stream.write(data)
+    data = wf.readframes(CHUNK)
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
